49a68e535beb30aab55c02732c05501e5288ad1b
Docker <docker@example.com>
2024-10-09 15:29:06 +0100

    swag

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -1,9 +1,26 @@
-let lang1 _ = failwith ""
+let lang1 l = 
+  (List.length (List.filter(fun x -> x = '0' || x = '1' ) l) > 0) && (List.length (List.filter(fun x -> x != '0' && x != '1' ) l) = 0)
+;;
 
-let lang2 _ = failwith ""
+let lang2 l = match l with 
+    [] -> true 
+  | a::l' -> if a = '0' then 
+        List.length( List.filter(fun x -> x = '1') l') = List.length l'
+      else if a = '1' then List.length( List.filter(fun x -> x = '1') l) = List.length l
+      else false
+;;
 
-let lang3 _ = failwith ""
+let first_l = function 
+  a::l -> a
+;;
 
+let rec last_l = function 
+  a::l -> last_l l
+  | a::[] -> a
+  | [] -> failwith"stringa vuota"
+;;
+
+let rec lang3 l = 
 let lang4 _ = failwith ""
 
 let lang5 _ = failwith ""
---
+++ basics/recognizer/test/test_recognizer.ml
@@ -0,0 +1,3 @@
+open Recognizer
+
+let%test _ = belongsTo ['0';'0';'1';'0'] = [true; false; true; false; false];;
